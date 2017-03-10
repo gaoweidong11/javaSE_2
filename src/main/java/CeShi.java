66ca5a77fd0b0c469445a5d4c14893ffcf508d7b
高伟冬 <280823086@qq.com>
2017-03-10 15:49:08 +0800

    update

---
+++ src/main/java/CeShi.java
@@ -0,0 +1,9 @@
+/**
+ * Created by 高伟冬 on 2017/3/10.
+ */
+public class CeShi {
+    public static void main(String[] args) {
+        int x=2, y=3, z=4;
+        System.out.println(z -= --y - x--);
+    }
+}
