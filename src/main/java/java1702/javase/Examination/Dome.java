c76d3b16500f14af553ef8e9d0cdf6681d5f80ea
高伟冬 <280823086@qq.com>
2017-03-20 13:41:21 +0800

    update

---
+++ src/main/java/java1702/javase/Examination/Dome.java
@@ -0,0 +1,17 @@
+package java1702.javase.Examination;
+
+/**
+ * Created by 高伟冬 on 2017/3/20.
+ * javaSE_2
+ * 10:21
+ * 星期一
+ */
+public class Dome {
+    public static void main(String[] args) {
+
+        DefaultClass defaultClass = new DefaultClass();
+
+        System.out.println(defaultClass);
+    }
+}
+
---
+++ src/main/java/java1702/javase/basic/Dome1.java
@@ -0,0 +1,13 @@
+package java1702.javase.basic;
+
+import java1702.javase.Examination.DefaultClass;
+
+/**
+ * Created by 高伟冬 on 2017/3/20.
+ */
+public class Dome1 {
+    public static void main(String[] args) {
+        DefaultClass defaultClass = new DefaultClass();
+        System.out.println(defaultClass);
+    }
+}
