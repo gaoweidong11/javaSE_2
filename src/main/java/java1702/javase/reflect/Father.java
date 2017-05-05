e97420d31a29b97bd5af0ba5964fe4ba3dad4b49
高伟冬 <280823086@qq.com>
2017-05-05 19:21:41 +0800

    update

---
+++ src/main/java/java1702/javase/reflect/Father.java
@@ -0,0 +1,19 @@
+package java1702.javase.reflect;
+/**
+ * Created by 高伟冬 on 2017/5/5.
+ * javaSE_2
+ * 15:21
+ * 星期五
+ */
+public class Father {
+
+}
+
+interface InterfaceText {
+
+}
+class Son extends Father implements InterfaceText{
+    public static void main(String[] args) {
+
+    }
+}
